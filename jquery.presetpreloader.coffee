e2241daa108dc025c44ef4c8fe2f435b7aef6806
Takazudo <takazudo@gmail.com>
2012-03-30 03:06:47 +0900

    implemented basic things

--- jquery.presetpreloader.coffee
+++ jquery.presetpreloader.coffee
@@ -1,5 +1,134 @@
 (($) -> # encapsulate plugin start
 
+  loader = null
+
+  # if with jQuery.ImgLoader, fetchImg send request only once per src
+  # https://github.com/Takazudo/jQuery.ImgLoader
+  fetchImg = $.ImgLoaderNs?.fetchImg or (src) ->
+    (new Image).src = src
+
+
+  # wraps one config item
+
+  class Item_replacer
+    constructor: (@replInfo) ->
+    applyReplace: (baseSrc) ->
+      if not @replInfo.expr.test baseSrc
+        return null
+      baseSrc.replace @replInfo.expr, @replInfo.result
+    load: (baseSrc) ->
+      src = @applyReplace baseSrc
+      if src then fetchImg src
+      @
+
+  class Item_srcs
+    constructor: (@srcs) ->
+    load: ->
+      $.each @srcs, (i, src) -> fetchImg src
+      @
+
+
+  # config manager
+  
+  class PresetPreloader
+
+    constructor: (config) ->
+
+      # handle instance creation wo new
+      if not (@ instanceof arguments.callee)
+        return new PresetPreloader config
+
+      @_config = {}
+      if config then @define config
+
+    define: (config) ->
+      $.each config, (key, val) =>
+        switch ($.type val)
+          when 'array'
+            @_config[key] = new Item_srcs val
+          when 'object'
+            @_config[key] = new Item_replacer val
+      @
+
+    get: (name) ->
+      item = @_config[name]
+      if not item
+        console?.log "PresetPreloader: #{name} was not defined yet"
+        return false
+      item
+
+    load: (name, src) ->
+      item = @get name
+      if not item
+        console?.log "PresetPreloadr: #{name} was tried to load but not defined yet"
+        return false
+      item.load src
+      @
+
+
+  # instancify loader
+  
+  loader = new PresetPreloader
+
+
+  # rollover item
+
+  class Rollover
+    
+    constructor: (@el, useActive) ->
+
+      @$watcher = $(@el)
+      if @$watcher.is 'img,:image'
+        @$img = @$watcher
+      else
+        @$img = @$watcher.find 'img,:image'
+
+      @_src_normal = @$img.attr 'src'
+      @_src_hover = (loader.get 'hoverimg').applyReplace @_src_normal
+      fetchImg @_src_hover
+
+      @$watcher.hover @toHover, @toNormal
+      
+    toNormal: =>
+      @$img.attr 'src', @_src_normal
+      @
+    toHover: =>
+      @$img.attr 'src', @_src_hover
+      @
+
+
+  # jQuery bridge
+
+  $.fn.presetPreload = ->
+    @each ->
+      $el = $(@)
+      key = $el.data 'preloadKey'
+      src = $el.attr 'src'
+      loader.load key, src
+
+  $.fn.rollover = ->
+    @each -> new Rollover @
+  
+
+  
+  # globalify
+  $.PresetPreloader = PresetPreloader
+  $.presetPreloader = loader
+
+
+  #loader.define
+  #  'foo': [ 'imgs/1.jpg', 'imgs/2.jpg', 'imgs/3.jpg' ]
+  #  'bar': [ 'imgs/4.jpg', 'imgs/5.jpg', 'imgs/6.jpg' ]
+  #  'hoverimg':
+  #    expr: /^(.+_)normal(\.[^.]+)$/
+  #    result:'$1hover$2'
+
+  #loader.load 'foo'
+  #loader.load 'bar'
+  #loader.load 'hoverimg', 'imgs/bothinsamedir/1_normal.jpg'
+  #loader.load 'hoverimg', 'imgs/bothinsamedir/2_normal.jpg'
+  #loader.load 'hoverimg', 'imgs/bothinsamedir/3_normal.jpg'
+
 
 ) jQuery # encapsulate plugin end
 
--- jquery.presetpreloader.js
+++ jquery.presetpreloader.js
@@ -1,6 +1,147 @@
-/*! jQuery.ui.domwindow - v0.1.3 -  3/29/2012
- * https://github.com/Takazudo/jQuery.ui.domwindow
+/*! jQuery.PresetPreloader - v0.1.3 -  3/30/2012
+ * https://github.com/Takazudo/jQuery.PresetPreloader
  * Copyright (c) 2012 "Takazudo" Takeshi Takatsudo; Licensed MIT */
 
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-(function($) {})(jQuery);
+(function($) {
+  var Item_replacer, Item_srcs, PresetPreloader, Rollover, fetchImg, loader, _ref;
+  loader = null;
+  fetchImg = ((_ref = $.ImgLoaderNs) != null ? _ref.fetchImg : void 0) || function(src) {
+    return (new Image).src = src;
+  };
+  Item_replacer = (function() {
+
+    function Item_replacer(replInfo) {
+      this.replInfo = replInfo;
+    }
+
+    Item_replacer.prototype.applyReplace = function(baseSrc) {
+      if (!this.replInfo.expr.test(baseSrc)) return null;
+      return baseSrc.replace(this.replInfo.expr, this.replInfo.result);
+    };
+
+    Item_replacer.prototype.load = function(baseSrc) {
+      var src;
+      src = this.applyReplace(baseSrc);
+      if (src) fetchImg(src);
+      return this;
+    };
+
+    return Item_replacer;
+
+  })();
+  Item_srcs = (function() {
+
+    function Item_srcs(srcs) {
+      this.srcs = srcs;
+    }
+
+    Item_srcs.prototype.load = function() {
+      $.each(this.srcs, function(i, src) {
+        return fetchImg(src);
+      });
+      return this;
+    };
+
+    return Item_srcs;
+
+  })();
+  PresetPreloader = (function() {
+
+    function PresetPreloader(config) {
+      if (!(this instanceof arguments.callee)) return new PresetPreloader(config);
+      this._config = {};
+      if (config) this.define(config);
+    }
+
+    PresetPreloader.prototype.define = function(config) {
+      var _this = this;
+      $.each(config, function(key, val) {
+        switch ($.type(val)) {
+          case 'array':
+            return _this._config[key] = new Item_srcs(val);
+          case 'object':
+            return _this._config[key] = new Item_replacer(val);
+        }
+      });
+      return this;
+    };
+
+    PresetPreloader.prototype.get = function(name) {
+      var item;
+      item = this._config[name];
+      if (!item) {
+        if (typeof console !== "undefined" && console !== null) {
+          console.log("PresetPreloader: " + name + " was not defined yet");
+        }
+        return false;
+      }
+      return item;
+    };
+
+    PresetPreloader.prototype.load = function(name, src) {
+      var item;
+      item = this.get(name);
+      if (!item) {
+        if (typeof console !== "undefined" && console !== null) {
+          console.log("PresetPreloadr: " + name + " was tried to load but not defined yet");
+        }
+        return false;
+      }
+      item.load(src);
+      return this;
+    };
+
+    return PresetPreloader;
+
+  })();
+  loader = new PresetPreloader;
+  Rollover = (function() {
+
+    function Rollover(el, useActive) {
+      this.el = el;
+      this.toHover = __bind(this.toHover, this);
+      this.toNormal = __bind(this.toNormal, this);
+      this.$watcher = $(this.el);
+      if (this.$watcher.is('img,:image')) {
+        this.$img = this.$watcher;
+      } else {
+        this.$img = this.$watcher.find('img,:image');
+      }
+      this._src_normal = this.$img.attr('src');
+      this._src_hover = (loader.get('hoverimg')).applyReplace(this._src_normal);
+      fetchImg(this._src_hover);
+      this.$watcher.hover(this.toHover, this.toNormal);
+    }
+
+    Rollover.prototype.toNormal = function() {
+      this.$img.attr('src', this._src_normal);
+      return this;
+    };
+
+    Rollover.prototype.toHover = function() {
+      this.$img.attr('src', this._src_hover);
+      return this;
+    };
+
+    return Rollover;
+
+  })();
+  $.fn.presetPreload = function() {
+    return this.each(function() {
+      var $el, key, src;
+      $el = $(this);
+      key = $el.data('preloadKey');
+      src = $el.attr('src');
+      return loader.load(key, src);
+    });
+  };
+  $.fn.rollover = function() {
+    return this.each(function() {
+      return new Rollover(this);
+    });
+  };
+  $.PresetPreloader = PresetPreloader;
+  return $.presetPreloader = loader;
+})(jQuery);
